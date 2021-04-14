5ab33859f2534f9569be8dc662c5c9fa132f60d7
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-14 10:04:06 -0700

    copying models from page to page to reedit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -36,5 +36,38 @@ router.get('/login', prevAuth, (req, res) => {
 
   res.render('login');
 });
+router.get('/post/:id', checkAuth, async (req, res) => {
+  const postData = await Post.findByPk(req.params.id, {
+      include: [{
+          model: User,
+          attributes: ['id', 'user_name'],
+      },]
+  });
+  //serialize post data
+  const post = postData.get();
+  //pulling out user name to where handlebars can grab it
+  const user = JSON.parse(JSON.stringify(post.user));
+  post.userName = user.user_name;
+
+
+  const commentData = await Comment.findAll({
+      where: { post_id: post.id },
+      include: {
+          model: User,
+          attributes: ['id', 'user_name'],
+      },
+  })
+
+  //serialize comment data
+  const comments = commentData.map((comment) => comment.get())
+  //pull out user names where handlebars can grab it
+  for (let i = 0; i < comments.length; i++) {
+      const user = comments[i].user;
+      comments[i].userName = user.user_name;
+  }
+
+
+  res.render('postpage', { post, comments, loggedIn: req.session.userId? true:false  })
+})
 
 module.exports = router;
---
+++ models/comments.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
